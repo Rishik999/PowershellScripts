6d0eca6fe5c7f3facd7ee7e4c2890e9742bb9aea
Rishik Bajaj <bajajrishi@gmail.com>
2020-02-03 20:04:32 +0530

    Initial commit

---
+++ Get-Azwebapps.ps1
@@ -0,0 +1,11 @@
+﻿<#Running this script will fetch all the details for Azure Web apps #>
+
+Import-module Az
+Connect-AzAccount
+Get-AzSubscription
+Select-AzSubscription -Tenantid "a384ed05-4d66-4ce7-937d-3dab0fb2d8d4"
+$subs=(Get-AzSubscription -Tenantid "a384ed05-4d66-4ce7-937d-3dab0fb2d8d4").name
+foreach($s in $subs){
+Select-AzSubscription -SubscriptionName $s
+Get-AzWebApp |select *, @{name="subscriptionname"; expression={$s}} | Export-Csv -Path c:\temp\webappdetails.csv -Append -NoTypeInformation
+}
